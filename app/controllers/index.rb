1bd336753a7223b217d22af5b796a1aab2da3143
Hethe Berg <growthcode@gmail.com>
2014-08-06 02:51:41 -0700

    trying to fix form on new pap page

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,10 +1,9 @@
-# need to fix CSS not displaying on new session page
+# Index Page
+# Index has sessions for creation
+
 get '/' do
   erb :index
-end
-
-get '/sessions/new' do
-  erb :sign_in
+#*******# need to fix CSS not displaying on new session page
 end
 
 post '/sessions' do
@@ -18,9 +17,35 @@ post '/sessions' do
   end
 end
 
+delete '/logout' do
+  session[:user_id] = nil
+  redirect '/'
+end
+
+post '/users/create' do
+  user = User.new(params)
+  user.save!
+  session[:user_id] = user.id
+  flash[:errors] = user.errors.full_messages
+
+  redirect '/paps'
+end
+
+##############################################################
+
+# gerneral info pages, not updated on their erbs
+
+get '/about' do
+  erb :about
+end
+
+##############################################################
+
+# PAPS_ALL PAGE, lists all user's Paps in an <ul>
+
 get '/paps' do
   @paps = current_user.paps.all
-  erb :paps
+  erb :paps_all
 end
 
 post '/paps/create' do
@@ -32,43 +57,12 @@ post '/paps/create' do
   redirect '/paps'
 end
 
-get '/about' do
-  erb :about
-end
+##############################################################
 
+get "/paps/:id/:process_name" do
+  @pap = Pap.find(params[:id])
 
-# get '/logout' do
-#   user = User.find_by_email(params[:email])
-#   @current_user.session.clear
-#   sessions[:user_id] = nil
-#   sessions.clear
-#   redirect '/'
-# end
-
-
-delete '/logout' do
-  session[:user_id] = nil
-  redirect '/'
-end
-
-# get '/user/new' do
-#   erb :sign_up
-# end
-
-post '/users/create' do
-  user = User.new(params)
-  user.save!
-  session[:user_id] = user.id
-  flash[:errors] = user.errors.full_messages
-
-  redirect '/paps'
-end
-
-
-get "/:process_name" do
-  @pap = Pap.find_by_process_name(params[:process_name])
-
-  erb :process
+  erb :a_process
 end
 
 post '/action/create' do
